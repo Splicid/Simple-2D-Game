0bf196c0fd56cbee45a71f234a99bb151b78ff9c
Splicid <luis1abreu11@gmail.com>
2023-11-21 21:47:27 -0500

    Still work in progress

--- BasicStuff/BasicStuff.cpp
+++ BasicStuff/BasicStuff.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 // Version 0.1
-
+// To Do
+// Make bullets shoot towards mouse
 
 int main()
 {
@@ -10,8 +11,15 @@ int main()
     sf::RectangleShape rectangle;
 
     // Setting the size and origin of the rectangle
-    rectangle.setSize(sf::Vector2f(100.0f, 100.0f));
-    rectangle.setOrigin(sf::Vector2f(50.0f, 50.0f));
+    rectangle.setSize(sf::Vector2f(50.0f, 50.0f));
+    rectangle.setOrigin(sf::Vector2f(25.0f, 25.0f));
+
+    sf::CircleShape circle;
+    circle.setFillColor(sf::Color::Red);
+    circle.setRadius(30.0f);
+    circle.setOrigin(30, 30);
+    std::vector<sf::CircleShape> bullets;
+    std::vector<float> angles;
 
     rectangle.setOutlineColor(sf::Color::Red);
     rectangle.setFillColor(sf::Color::Green);
@@ -23,14 +31,16 @@ int main()
         window.setFramerateLimit(120);
         sf::Event event;
         sf::Vector2f pos = rectangle.getPosition();
+
         // Get postion of mouse 
         sf::Vector2i curPos = sf::Mouse::getPosition(window);
         float dx = curPos.x - pos.x;
         float dy = curPos.y - pos.y;
         float rotation = (atan2(dy, dx)) * 180 / PI;
 
+        std::vector<sf::Vector2f>position;
 
-        std::cout << pos.x << "," << pos.y << " " << window.getSize().y << " " << curPos.x << std::endl;
+        //std::cout << pos.x << "," << pos.y << " " << window.getSize().y << " " << curPos.x << std::endl;
         rectangle.setRotation(rotation);
 
         while (window.pollEvent(event))
@@ -61,11 +71,25 @@ int main()
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space))
         {
-            //Gun Switch
-         
+            bullets.push_back(sf::CircleShape());
+            bullets.back().setRadius(5);
+            bullets.back().setOrigin(5, 5);
+            bullets.back().setPosition(rectangle.getPosition());
+
+
+            //for (sf::Vector2f i : position)
+            //{
+            //    std::cout << " Start x" << i.x << " " << " End Y" << i.y;
+            //}
         }
+
         window.clear();
         window.draw(rectangle);
+        for (int i = 0; i < bullets.size(); i++)
+        {
+            window.draw(bullets[i]);
+            bullets[i].move(0.0f, 1.0f);
+        }
         window.display();
     }
 
